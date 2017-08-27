e1de51c1cc803305824b92af6153df8ac8aef5c8
Abraham P Chaffin <abraham@apc.cc>
2017-08-27 16:03:11 -0700

    Update restoreFilesFromStorageArchive.sh

--- scripts/tools/restoreFilesFromStorageArchive.sh
+++ scripts/tools/restoreFilesFromStorageArchive.sh
@@ -1,6 +1,35 @@
 #!/bin/bash
 
+SOURCE_ARCHIVE="$1"
+if [ -z "$SOURCE_ARCHIVE" ];then
+ echo "Source archive not set!"
+ exit 0
+fi
+DESTINATION_DIRECTORY="$2"
+if [ -z "$DESTINATION_DIRECTORY" ];then
+ echo "Destination directory not set!"
+ exit 0
+fi
+if [ ! -d "$DESTINATION_DIRECTORY" ];then
+ echo "Destination directory does not exist!"
+ exit 0
+fi
+
 echo "Restoring Files From Storage Archive";
+echo "Source: $SOURCE_ARCHIVE"
+echo "Destination: $DESTINATION_DIRECTORY"
+
+cd "$DESTINATION_DIRECTORY"
+echo "Copying File..."
+gsutil cp "gs://${SOURCE_ARCHIVE}" .
+DESTINATION_FILE=`basename "${SOURCE_ARCHIVE}"`
+DESTINATION_FILE_PATH="${DESTINATION_DIRECTORY}/${DESTINATION_FILE}"
+if [ ! -f "${DESTINATION_FILE_PATH}" ];then
+ echo "File did not transfer!"
+ exit 0
+fi
+echo "Extracting file..."
+tar -xzf "${DESTINATION_FILE}"
 
 echo "Done Restoring Files From Storage Archive";
 
