8fe2995090e5c8c10a1146ef419624497148f2ed
Abraham P Chaffin <abraham@apc.cc>
2017-08-27 16:32:00 -0700

    Update setup.sh

--- scripts/buildLocalServer/setup.sh
+++ scripts/buildLocalServer/setup.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 #Setup the system
+if [ ! -d ~/setup-config/setup_gcp ];then
+ echo "Creating ~/setup-config/setup_gcp"
+ mkdir -p ~/setup-config/setup_gcp;
+fi
+if [ ! -f ~/setup-config/setup_gcp/core.sh ];then
+ cp ~/setup_gcp/settings/core.template.sh ~/setup-config/setup_gcp/core.sh
+fi
 
 
 exit 0
