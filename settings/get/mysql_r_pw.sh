2c3a5356e2ce21c9592c54c0baa675d3ef075bde
Abraham P Chaffin <abraham@apc.cc>
2017-08-27 17:10:12 -0700

    Update mysql_r_pw.sh

--- settings/get/mysql_r_pw.sh
+++ settings/get/mysql_r_pw.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-#script for scripts to quickly get the mysql root password - if they have access to this then they're already in too far anyway :(
-
-source ~/setup/settings/core.sh
-
+#script for scripts to quickly get the mysql root password
+source ~/setup-config/setup_gcp/core.sh
 echo "$MYSQL_ROOT_PW"
-
 exit 0
