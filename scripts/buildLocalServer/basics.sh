db54e7fe7bd93a9016c0daf2d874a40da8fe7739
Abraham P Chaffin <abraham@apc.cc>
2017-08-27 17:05:02 -0700

    Update basics.sh

--- scripts/buildLocalServer/basics.sh
+++ scripts/buildLocalServer/basics.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-source ~/setup/settings/core.sh
+source ~/setup-config/setup_gcp/core.sh
 
 echo "* Setting Up Basic System Settings for $COMPANY_NAME"
 echo "****************************************************"
 
 echo "* Updating apt"
-sudo apt-get update
+sudo apt-get update -qq
 
 echo "* Installing these basic packages:"
-cat ~/setup/settings/basics.packages.txt
+cat ~/setup_gcp/settings/basics.packages.txt
 echo "* Installing with yes set for all answers:"
-sudo apt-get install -y --force-yes `cat ~/setup/settings/basics.packages.txt | tr "\n" " "`
+sudo apt-get install -yqq --force-yes `cat ~/setup_gcp/settings/basics.packages.txt | tr "\n" " "`
 
 exit 0
