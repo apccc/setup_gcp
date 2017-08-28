296262a31236424fec8bf0c53e2eade08bf7ea44
Abraham P Chaffin <abraham@apc.cc>
2017-08-27 17:30:22 -0700

    Update mysql.sh

--- scripts/buildLocalServer/mysql.sh
+++ scripts/buildLocalServer/mysql.sh
@@ -3,6 +3,7 @@
 source ~/setup-config/setup_gcp/core.sh
 
 echo "* Setting up MySQL Server for $COMPANY_NAME on $HOSTNAME"
-~/setup_gcp/scripts/buildLocalServer/mysql/installMySQLServer.exp
+sudo apt-get install -yqq mysql-server
+sudo mysqladmin -u root password $MYSQL_ROOT_PW
 echo "* Done setting up MySQL Server"
 exit 0
