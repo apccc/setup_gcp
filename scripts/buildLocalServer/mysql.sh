7051a138f2ed67eafb3f5b38573c2e35795e70be
Abraham P Chaffin <abraham@apc.cc>
2017-08-28 17:49:09 -0700

    Update mysql.sh

--- scripts/buildLocalServer/mysql.sh
+++ scripts/buildLocalServer/mysql.sh
@@ -2,8 +2,13 @@
 
 source ~/setup-config/setup_gcp/core.sh
 
+if [ -z "$MYSQL_ROOT_PW" ];then
+ echo "MySQL root password not set!"
+ exit 1
+fi
+
 echo "* Setting up MySQL Server for $COMPANY_NAME on $HOSTNAME"
 sudo apt-get install -yqq mysql-server
-sudo mysqladmin -u root password $MYSQL_ROOT_PW
+sudo mysql-u root -e "DROP USER 'root'@'localhost';CREATE USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PW}';GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' with grant option;FLUSH PRIVILEGES;"
 echo "* Done setting up MySQL Server"
 exit 0
