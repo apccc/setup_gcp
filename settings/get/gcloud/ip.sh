74a256a3e61fcd3f630a7c18da6f8ef6bf8057cd
Abraham P Chaffin <abraham@apc.cc>
2017-08-28 18:13:48 -0700

    Update ip.sh

--- settings/get/gcloud/ip.sh
+++ settings/get/gcloud/ip.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcloud compute instances describe `hostname` --zone=$(~/setup/settings/get/gcloud/zone.sh) | grep natIP | egrep -oe '[0-9.]+$'
+gcloud compute instances describe `hostname` --zone=$(~/setup_gcp/settings/get/gcloud/zone.sh) | grep natIP | egrep -oe '[0-9.]+$'
 exit 0
