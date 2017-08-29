e6ad6eeb0a79715385c8a59dc5e90b1478a74141
Abraham P Chaffin <abraham@apc.cc>
2017-08-28 18:25:45 -0700

    Update setupDefaultSite.sh

--- scripts/buildLocalServer/apache2Web/setupDefaultSite.sh
+++ scripts/buildLocalServer/apache2Web/setupDefaultSite.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source ~/setup_gcp/settings/core.sh
+source ~/setup-config/setup_gcp/core.sh
 
 echo "* Building the default site ${COMPANY_ADMIN_SUBDOMAIN}.${COMPANY_DOMAIN}"
 echo "****************************"
