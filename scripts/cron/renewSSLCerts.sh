4f755736c1c4bcd8a56a0a7c66e5e62c8a9d31e4
Abraham P Chaffin <abraham@apc.cc>
2017-08-28 18:11:14 -0700

    Update renewSSLCerts.sh

--- scripts/cron/renewSSLCerts.sh
+++ scripts/cron/renewSSLCerts.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-source ~/setup/settings/core.sh
+source ~/setup-config/setup_gcp/core.sh
 
 echo " * Renewing SSL Certs"
 
 $MY 'UPDATE `'"$SYSTEM_DATABASE"'`.`sites` SET `renew_SSL`="T" WHERE `SSL`="T"'
 
-~/setup/scripts/buildLocalServer/apache2Web/setupDefaultSite.sh
-~/setup/scripts/buildLocalServer/apache2Web/setupServerSites.sh
+~/setup_gcp/scripts/buildLocalServer/apache2Web/setupDefaultSite.sh
+~/setup_gcp/scripts/buildLocalServer/apache2Web/setupServerSites.sh
 
 $MY 'UPDATE `'"$SYSTEM_DATABASE"'`.`sites` SET `renew_SSL`="F" WHERE `renew_SSL`="T"'
 
