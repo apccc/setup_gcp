14d9cc56667a9cdad103cd2db5b92314398e350c
Abraham P Chaffin <abraham@apc.cc>
2017-11-03 14:04:17 -0700

    Update transferGCSBackupFilesToFolder.sh

--- scripts/tools/datamanagement/transferGCSBackupFilesToFolder.sh
+++ scripts/tools/datamanagement/transferGCSBackupFilesToFolder.sh
@@ -1,6 +1,41 @@
 #!/bin/bash
 #Grab Google Cloud Storage backup files and set them up in a folder.
 
+if [ -z "$1" ];then
+  echo "Error: GCS source object path not provided."
+  exit 1
+fi
+GCSSOURCEPATH="$1"
 
+if [ ! -z "$2" ];then
+  DESTINATIONFOLDER="$2"
+else
+  DESTINATIONFOLDER="$PWD"
+fi
+
+if [ ! -d "$DESTINATIONFOLDER" ];then
+  mkdir "$DESTINATIONFOLDER";
+fi
+RAND=`cat /dev/urandom | tr -dc _A-Z-a-z-0-9 | head -c${1:-32}`
+TMPFILEFOLDER="/tmp/setupgcstrans_${RAND}"
+
+#make tmp folder
+if [ ! -d "$TMPFILEFOLDER" ];then
+  mkdir "$TMPFILEFOLDER"
+fi
+
+#download to tmp folder
+echo "Downloading ${GCSSOURCEPATH} to ${TMPFILEFOLDER}/";
+gsutil cp gs://${GCSSOURCEPATH} "${TMPFILEFOLDER}/"
+
+#locate file from tmp folder
+
+#extract file from tmp folder
+#tar xf file.tar.gz -C "${DESTINATIONFOLDER}"
+
+#clean up
+if [ -d "$TMPFILEFOLDER" ];then
+  echo "Cleaning up..."
+fi
 
 exit 0
