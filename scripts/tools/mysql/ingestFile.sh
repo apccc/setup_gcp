244c41408b4c9f51285ae28d034d747905f07c81
Abraham P Chaffin <abraham@apc.cc>
2017-11-03 15:33:30 -0700

    Update ingestFile.sh

--- scripts/tools/mysql/ingestFile.sh
+++ scripts/tools/mysql/ingestFile.sh
@@ -18,25 +18,8 @@ echo "File found: $BASEFILENAME"
 FILEEXT="${BASEFILENAME##*.}"
 echo "File extension found: $FILEEXT"
 
-TS=`date +%s`
-TMPFILEFOLDER="/tmp/setupmysqlingest$TS"
-
 if [ "$FILEEXT" == 'gz' ];then
-  #make tmp folder
-  if [ ! -d "$TMPFILEFOLDER" ];then
-    echo "Making $TMPFILEFOLDER"
-    mkdir "$TMPFILEFOLDER"
-  fi
-
-  echo "Extracting file ${INCOMINGFILE} to ${TMPFILEFOLDER}"
-  tar xf "${INCOMINGFILE}" -C "${TMPFILEFOLDER}"
-
-  #locate file from tmp folder
-  EXTRACTEDFILE=`ls -1 "${TMPFILEFOLDER}/"`
-  echo "Extracted file: ${EXTRACTEDFILE}"
-
-  #Use new file
-  MYSQLINPUTFILEPATH="${TMPFILEFOLDER}/${EXTRACTEDFILE}"
+  zcat "$INCOMINGFILE" | mysql -u root -p
 else
   MYSQLINPUTFILEPATH="${INCOMINGFILE}"
 fi
