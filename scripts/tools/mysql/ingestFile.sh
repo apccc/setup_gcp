1b7238a8ce0886ce1d6eaa17a78ac8e8f1732c6c
Abraham P Chaffin <abraham@apc.cc>
2017-11-03 15:16:26 -0700

    Create ingestFile.sh

---
+++ scripts/tools/mysql/ingestFile.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ -z "$1" ];then
+  echo "File not set!"
+  exit 1
+fi
+
+if [ ! -f "$1" ];then
+  echo "No file found $1"
+  exit 1
+fi
+
+INCOMINGFILE="$1"
+
+BASEFILENAME=`basename ${INCOMINGFILE}`
+
+
+echo "Ingesting MySQL File"
+
+exit 0
