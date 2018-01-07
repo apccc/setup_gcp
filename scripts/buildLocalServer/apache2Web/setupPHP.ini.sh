98ffd1470f5b6d92a76eb208e2fc75ce1bfbd4b1
Abraham P Chaffin <abraham@apc.cc>
2018-01-06 20:33:24 -0800

    Update setupPHP.ini.sh

--- scripts/buildLocalServer/apache2Web/setupPHP.ini.sh
+++ scripts/buildLocalServer/apache2Web/setupPHP.ini.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 source ~/setup-config/setup_gcp/core.sh
 
+PHPINI="/etc/php/7.0/apache2/php.ini"
+AUTO_PREPEND_FILE="/var/www/auto_preprend_file.php"
+
+echo " * Configuring php.ini"
+
+#setup the auto_prepend_file
+if [ ! -f "$AUTO_PREPEND_FILE" ];then
+  
+fi
+
+$FR "auto_prepend_file =" "auto_prepend_file = $AUTO_PREPEND_FILE" "$PHPINI"
 exit 0
