8752c15a47d815226c208575cfeb3d978343a7ec
Abraham P Chaffin <abraham@apc.cc>
2018-01-06 20:53:03 -0800

    Update setupPHP.ini.sh

--- scripts/buildLocalServer/apache2Web/setupPHP.ini.sh
+++ scripts/buildLocalServer/apache2Web/setupPHP.ini.sh
@@ -7,17 +7,15 @@ AUTO_PREPEND_FILE="/var/www/auto_preprend_file.php"
 echo " * Configuring php.ini"
 
 #setup the auto_prepend_file
-if [ ! -f "$AUTO_PREPEND_FILE" ];then
-  F="$AUTO_PREPEND_FILE"
-  echo '<?php' | sudo tee "$F" > /dev/null
-  echo '$MYSQL_USER="'"$MYSQL_WEB_USER"'";' | sudo tee -a "$F" > /dev/null
-  echo '$MYSQL_PASS="'"$MYSQL_WEB_USER_PASS"'";' | sudo tee -a "$F" > /dev/null
-  echo '$RECAPTCHA_SITE_KEY="'"$RECAPTCHA_SITE_KEY"'";' | sudo tee -a "$F" > /dev/null
-  echo '$RECAPTCHA_SECRET_KEY="'"$RECAPTCHA_SECRET_KEY"'";' | sudo tee -a "$F" > /dev/null
-  echo '$MAILGUN_SMTP_USERNAME="'"$MAILGUN_SMTP_USERNAME"'";' | sudo tee -a "$F" > /dev/null
-  echo '$MAILGUN_SMTP_PASSWORD="'"$MAILGUN_SMTP_PASSWORD"'";' | sudo tee -a "$F" > /dev/null
-  echo '?>' | sudo tee -a $F > /dev/null
-fi
+F="$AUTO_PREPEND_FILE"
+echo '<?php' | sudo tee "$F" > /dev/null
+echo '$MYSQL_USER="'"$MYSQL_WEB_USER"'";' | sudo tee -a "$F" > /dev/null
+echo '$MYSQL_PASS="'"$MYSQL_WEB_USER_PASS"'";' | sudo tee -a "$F" > /dev/null
+echo '$RECAPTCHA_SITE_KEY="'"$RECAPTCHA_SITE_KEY"'";' | sudo tee -a "$F" > /dev/null
+echo '$RECAPTCHA_SECRET_KEY="'"$RECAPTCHA_SECRET_KEY"'";' | sudo tee -a "$F" > /dev/null
+echo '$MAILGUN_SMTP_USERNAME="'"$MAILGUN_SMTP_USERNAME"'";' | sudo tee -a "$F" > /dev/null
+echo '$MAILGUN_SMTP_PASSWORD="'"$MAILGUN_SMTP_PASSWORD"'";' | sudo tee -a "$F" > /dev/null
+echo '?>' | sudo tee -a $F > /dev/null
 
 #perform updates on php.ini file
 TMPFILE=/tmp/setupphpini.txt
