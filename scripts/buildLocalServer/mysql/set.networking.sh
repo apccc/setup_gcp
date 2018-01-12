f2a5bd96361e6ca0c44eb98a07afce65fc78d638
Abraham P Chaffin <abraham@apc.cc>
2018-01-12 15:56:29 -0800

    Update set.networking.sh

--- scripts/buildLocalServer/mysql/set.networking.sh
+++ scripts/buildLocalServer/mysql/set.networking.sh
@@ -8,12 +8,12 @@ else
   BINDADDRESS='127.0.0.1'
 fi
 
-echo " * Setting bind-address to $BINDADDRESS"
+F="/etc/mysql/setup.conf.d/networking.cnf"
+echo " * Setting bind-address to $BINDADDRESS in $F"
 
 #make sure the setup.conf.d directory is setup
 ~/setup_gcp/scripts/buildLocalServer/mysql/setup.conf.d.sh
 
-F="/etc/mysql/setup.conf.d/networking.cnf"
 echo "[mysqld]" | sudo tee "$F" > /dev/null
 echo "bind-address=$BINDADDRESS" | sudo tee -a "$F" > /dev/null
 
