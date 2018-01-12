5da75dff0848ea963f751b6d3b57d2c62cf95247
Abraham P Chaffin <abraham@apc.cc>
2018-01-12 15:34:45 -0800

    Update set.replication.master.sh

--- scripts/buildLocalServer/mysql/set.replication.master.sh
+++ scripts/buildLocalServer/mysql/set.replication.master.sh
@@ -5,6 +5,11 @@
 #make sure the setup.conf.d directory is setup
 ~/setup_gcp/scripts/buildLocalServer/mysql/setup.conf.d.sh
 
-
+#begin setting up the replication master
+F='/etc/mysql/setup.conf.d/replicationmaster.cnf'
+if [ ! -f "$F" ];then
+  echo "Creating $F"
+  echo "" | sudo -t "$F"
+fi
 
 exit 0
