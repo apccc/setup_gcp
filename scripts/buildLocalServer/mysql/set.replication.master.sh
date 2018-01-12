6efa9b0ac96b7457560c61c14d4ec2a2dd9dd796
Abraham P Chaffin <abraham@apc.cc>
2018-01-12 15:43:54 -0800

    Making file executable.

--- scripts/buildLocalServer/mysql/set.replication.master.sh
+++ scripts/buildLocalServer/mysql/set.replication.master.sh
@@ -2,14 +2,18 @@
 
 #set the database server to act as a replication master
 
-#make sure the setup.conf.d directory is setup
-~/setup_gcp/scripts/buildLocalServer/mysql/setup.conf.d.sh
-
 #begin setting up the replication master
 F='/etc/mysql/setup.conf.d/replicationmaster.cnf'
 if [ ! -f "$F" ];then
-  echo "Creating $F"
-  echo "" | sudo -t "$F"
+  echo " * Setting up server database replication as master."
+
+  #make sure the setup.conf.d directory is setup
+  ~/setup_gcp/scripts/buildLocalServer/mysql/setup.conf.d.sh
+  echo " * Creating $F"
+  echo "[mysqld]" | sudo tee "$F" > /dev/null
+  echo "log-bin" | sudo tee -a "$F" > /dev/null
+  SERVERID=`echo $((1 + RANDOM % 1000000))`
+  echo "server_id=$SERVERID" | sudo tee -a "$F" > /dev/null
 fi
 
 exit 0
