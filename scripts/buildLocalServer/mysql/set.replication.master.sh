00d1a2614a1997772f61365eb9f7eb176856a0f4
Abraham P Chaffin <abraham@apc.cc>
2018-01-12 15:32:55 -0800

    Update set.replication.master.sh

--- scripts/buildLocalServer/mysql/set.replication.master.sh
+++ scripts/buildLocalServer/mysql/set.replication.master.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
 
+#set the database server to act as a replication master
+
+#make sure the setup.conf.d directory is setup
+~/setup_gcp/scripts/buildLocalServer/mysql/setup.conf.d.sh
+
+
+
+exit 0
