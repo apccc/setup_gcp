188105407cf27fa44f8ec702b76c608a0917b1db
Abraham P Chaffin <abraham@apc.cc>
2018-01-12 16:43:40 -0800

    Update getMasterStatus.sh

--- scripts/buildLocalServer/mysql/getMasterStatus.sh
+++ scripts/buildLocalServer/mysql/getMasterStatus.sh
@@ -4,6 +4,6 @@
 
 source ~/setup-config/setup_gcp/core.sh
 
-mysql -u root -p"$MYSQL_ROOT_PW" -e "SHOW MASTER STATUS;" | grep mysql-bin | sed 's/\s\+/ /g' | sed 's/\s$//'
+mysql -u root -p"$MYSQL_ROOT_PW" -e "SHOW MASTER STATUS;" | grep bin | sed 's/\s\+/ /g' | sed 's/\s$//'
 
 exit 0
