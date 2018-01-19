72364504e3c469e5a80209a6f9387b69cfd009e4
Abraham P Chaffin <abraham@apc.cc>
2018-01-19 15:52:27 -0800

    Update buildLocalSlaveFromMasterBackupFile.sh

--- scripts/tools/mysql/buildLocalSlaveFromMasterBackupFile.sh
+++ scripts/tools/mysql/buildLocalSlaveFromMasterBackupFile.sh
@@ -2,6 +2,18 @@
 
 #build a local MySQL slave from master backup file
 
+if [ -z "$1" ];then
+  echo "File not set!"
+  exit 1
+fi
+
+if [ ! -f "$1" ];then
+  echo "No file found $1"
+  exit 1
+fi
+
+source ~/setup-config/setup_gcp/core.sh
+
 
 
 #turn off external connections while we work
