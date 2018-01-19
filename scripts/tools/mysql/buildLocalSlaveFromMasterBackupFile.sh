0d1c74234b4501938a821e7e5948fb280b235cc7
Abraham P Chaffin <abraham@apc.cc>
2018-01-19 15:51:26 -0800

    Create buildLocalSlaveFromMasterBackupFile.sh

---
+++ scripts/tools/mysql/buildLocalSlaveFromMasterBackupFile.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#build a local MySQL slave from master backup file
+
+#turn off external connections while we work
+
+#turn off any existing slave working
+
+#ingest the data
+
+#setup replication information
+
+#start the slave back up
+
+
+
+exit 0
