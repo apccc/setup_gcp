3fa6492963772a0feeb20f50591585065e0c7466
Abraham P Chaffin <abraham@apc.cc>
2018-01-19 14:22:13 -0800

    Making file executable.

---
+++ scripts/buildLocalServer/mysql/set.replication.slavedatabasesfromgz.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+if [ -z "$1" ];then
+  echo " * setReplicationDatabasesFromGZBackup - File not set!"
+  exit 1
+fi
+
+#set file path
+F=$1
+
+if [ ! -f "$F" ];then
+  echo " * setReplicationDatabasesFromGZBackup - File not found!"
+  exit 1
+fi
+
+#config variables
+CONFIGFILEPATH='/etc/mysql/setup.conf.d/replication_dbs.cnf'
+
+#make sure the setup.conf.d directory is setup
+~/setup_gcp/scripts/buildLocalServer/mysql/setup.conf.d.sh
+
+echo " * Setting up $CONFIGFILEPATH from $F"
+echo "[mysqld]" | sudo tee "$CONFIGFILEPATH" > /dev/null
+
+replicate-wild-do-table=setup_system.%
+#loop through databases in backup file
+for D in `zcat "$F" | egrep -e '^CREATE DATABASE' | egrep -oe '`[a-zA-Z0-9_]*`' | egrep -oe '[a-zA-Z0-9_]*'`;do
+  echo "replicate-wild-do-table=${D}.%" | sudo tee -a "$CONFIGFILEPATH" > /dev/null
+done
+
+exit 0;
