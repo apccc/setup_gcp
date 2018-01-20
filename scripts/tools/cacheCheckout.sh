c275e6e3243559c84a7b109bca27252531b9d3da
Abraham P Chaffin <abraham@apc.cc>
2018-01-20 15:22:51 -0800

    Update cacheCheckout.sh

--- scripts/tools/cacheCheckout.sh
+++ scripts/tools/cacheCheckout.sh
@@ -6,16 +6,21 @@ if [ -z "$1" ];then
   echo " * Error: cacheCheckout - No key provided!"
   exit 1
 fi
-if [ -z "$2" ];then
-  echo " * Error: cacheCheckout - No days provided!"
+KEY=`echo "$1" | egrep -oe '[a-zA-Z0-9_]*' | head -n 1`
+if [ -z "$KEY" ];then
+  echo " * Error: cacheCheckout - Bad key provided!"
   exit 1
 fi
-if [ -z "$3" ];then
+#only a key provided, try to output the data
+if [ -z "$2" ];then
+  $MY "SELECT data FROM ${SYSTEM_DATABASE}.cache WHERE k='"$KEY"' AND expires > NOW() LIMIT 1" | tail -n +2 | base64 -d
+  exit 0
+fi
+if [ ! -z "$2" ] && [ -z "$3" ];then
   echo " * Error: cacheCheckout - No data provided!"
   exit 1
 fi
 
-KEY=`echo "$1" | egrep -oe '[a-zA-Z0-9_]*' | head -n 1`
 DAYS=`echo "$2" | egrep -oe '[0-9]*' | head -n 1`
 
 source ~/setup-config/setup_gcp/core.sh
