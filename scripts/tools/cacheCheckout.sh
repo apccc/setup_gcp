c0015d0aa39640a3c88ac9959684666b9be9684d
Abraham P Chaffin <abraham@apc.cc>
2018-01-20 14:54:30 -0800

    Update cacheCheckout.sh

--- scripts/tools/cacheCheckout.sh
+++ scripts/tools/cacheCheckout.sh
@@ -20,15 +20,15 @@ DAYS=`echo "$2" | egrep -oe '[0-9]*' | head -n 1`
 
 source ~/setup-config/setup_gcp/core.sh
 
-#fail if there is an existing entry, other wise store and output okay
+#help cleanup
+$MY "DELETE FROM ${SYSTEM_DATABASE}.cache WHERE expires <= NOW() LIMIT 10000"
+
+#fail if there is an existing entry, otherwise store and output okay
 if [[ `$MY "SELECT id FROM ${SYSTEM_DATABASE}.cache WHERE k='"$KEY"' AND expires > NOW() LIMIT 1" | wc -l` -gt 0 ]];then
   echo 'fail'
 else
   $MY "INSERT INTO ${SYSTEM_DATABASE}.cache (k,expires,data) VALUES('"$KEY"',ADDDATE(NOW(), INTERVAL "$DAYS" DAY),'"$(echo "$3" | base64)"')"
   echo 'ok'
 fi
-
-#help cleanup
-$MY "DELETE FROM ${SYSTEM_DATABASE}.cache WHERE expires <= NOW() LIMIT 10000"
 
 exit 0
