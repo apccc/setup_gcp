ec137bd51332081737e3bdd159ce5b1a50399404
Abraham P Chaffin <abraham@apc.cc>
2018-01-20 14:47:09 -0800

    Update cacheCheckout.sh

--- scripts/tools/cacheCheckout.sh
+++ scripts/tools/cacheCheckout.sh
@@ -21,7 +21,7 @@ DAYS=`echo "$2" | egrep -oe '[0-9]*' | head -n 1`
 source ~/setup-config/setup_gcp/core.sh
 
 #fail if there is an existing entry, other wise store and output okay
-if [[ `$MY "SELECT id FROM ${SYSTEM_DATABASE}.cache WHERE key='"$KEY"' AND expires > NOW() LIMIT 1" | wc -l` -gt 0 ]];then
+if [[ `$MY "SELECT id FROM ${SYSTEM_DATABASE}.cache WHERE k='"$KEY"' AND expires > NOW() LIMIT 1" | wc -l` -gt 0 ]];then
   echo 'fail'
 else
   $MY "INSERT INTO ${SYSTEM_DATABASE}.cache (k,expires,data) VALUES('"$KEY"',ADDDATE(NOW(), INTERVAL "$DAYS" DAY),'"$($3 | base64)"')"
