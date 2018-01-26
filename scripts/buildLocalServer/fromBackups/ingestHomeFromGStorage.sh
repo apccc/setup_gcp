9166e3bab53b78af710d18dcab14e02ed5c6c61f
Abraham P Chaffin <abraham@apc.cc>
2018-01-26 15:56:01 -0800

    Update ingestHomeFromGStorage.sh

--- scripts/buildLocalServer/fromBackups/ingestHomeFromGStorage.sh
+++ scripts/buildLocalServer/fromBackups/ingestHomeFromGStorage.sh
@@ -11,40 +11,8 @@ GSBACKUPFILEFULLPATH="$1"
 
 echo " * Ingesting home directory from GS backup file $GSBACKUPFILEFULLPATH"
 
-GSBACKUPPATH=`echo "$GSBACKUPFILEFULLPATH" | cut -d'/' -f3,4`
-if [ -z "$GSBACKUPPATH" ];then
-  echo " * GS backup path unrecognized!"
-  exit 1
-fi
+~/setup_gcp/scripts/buildLocalServer/fromBackups/ingestFolderSkipExisting.sh "$GSBACKUPFILEFULLPATH" ~
 
-TMPDIR=/tmp/ingesthomefromgstore
-if [ ! -d "$TMPDIR" ];then
-  mkdir "$TMPDIR"
-fi
-~/setup_gcp/scripts/tools/datamanagement/transferGCSBackupFilesToFolder.sh "$GSBACKUPPATH" "$TMPDIR"
-
-SUBFOLDER=`ls -1 "$TMPDIR" | head -n 1`
-if [ ! -z "$SUBFOLDER" ] && [ -d "${TMPDIR}/${SUBFOLDER}" ];then
-  SOURCEHOMEPATH="${TMPDIR}/${SUBFOLDER}"
-  for F in `ls -1 "$SOURCEHOMEPATH"`;do
-    DEST=~/$F
-    if [ -d "$DEST" ];then
-      echo " * $DEST exists (directory) skipping..."
-      continue
-    fi
-    if [ -f "$DEST" ];then
-      echo " * $DEST exists (file) skipping..."
-      continue
-    fi
-    echo " * Moving $F to $DEST"
-    mv "${SOURCEHOMEPATH}/${F}" "$DEST"
-  done
-fi
-
-if [ -d "$TMPDIR" ];then
-  rm -rf "$TMPDIR"
-fi
-
-echo " * Done ingesting home directory from GS backup file $GSBACKUPFILE"
+echo " * Done ingesting home directory from GS backup file $GSBACKUPFILEFULLPATH"
 
 exit 0
