d941faf230af259101cb9f0d9aa106194ad0820d
Abraham P Chaffin <abraham@apc.cc>
2018-01-26 14:21:44 -0800

    Update fromBackups.sh

--- scripts/buildLocalServer/fromBackups.sh
+++ scripts/buildLocalServer/fromBackups.sh
@@ -6,12 +6,15 @@ if [ -z "$1" ];then
   echo "zstore not set!"
   exit 1
 fi
-ZSTORE="$1"
+ZSTORE=echo "$1" | egrep -oe '[0-9a-zA-Z_]*'
 FOLDERS="$2"
 
 echo " * Building local server from $ZSTORE backups!"
 
-
+MOST_RECENT_HOME_BACKUP=`gsutil ls -l "gs://$ZSTORE_home/" | egrep -ve '^TOTAL:' | awk '{print $2," ",$3}' | sort | tail -n 1 | awk '{print $2}'`
+if [ ! -z "$MOST_RECENT_HOME_BACKUP" ];then
+  echo " * Most recent home backup found: $MOST_RECENT_HOME_BACKUP"
+fi
 
 echo " * Done building local server from $ZSTORE backups!"
 exit 0
