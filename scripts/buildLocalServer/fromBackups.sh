779168e654a656023698701a23376d6767f21c2d
Abraham P Chaffin <abraham@apc.cc>
2018-01-26 15:47:11 -0800

    Update fromBackups.sh

--- scripts/buildLocalServer/fromBackups.sh
+++ scripts/buildLocalServer/fromBackups.sh
@@ -11,10 +11,32 @@ FOLDERS="$2"
 
 echo " * Building local server from $ZSTORE backups!"
 
+#grab the home directory from backup
 MOST_RECENT_HOME_BACKUP=`gsutil ls -l "gs://${ZSTORE}_home/" | egrep -ve '^TOTAL:' | awk '{print $2," ",$3}' | sort | tail -n 1 | awk '{print $2}'`
 if [ ! -z "$MOST_RECENT_HOME_BACKUP" ];then
   echo " * Most recent home backup found: $MOST_RECENT_HOME_BACKUP"
   ~/setup_gcp/scripts/buildLocalServer/fromBackups/ingestHomeFromGStorage.sh "$MOST_RECENT_HOME_BACKUP"
+fi
+
+#ingest the SSL certs, if there are any
+if [ -d ~/ssl_backup ];then
+  echo " * SSL certs found, will restore"
+  sudo mkdir /root/ssl 2>/dev/null
+  sudo rsync -av ~/ssl_backup/ /root/ssl/
+  sudo chown -R root:root /root/ssl/
+fi
+
+#ingest additional folders, if set to
+if [ ! -z "$FOLDERS" ];then
+  echo " * Grabbing additional folders from backup:"
+  for FOLDER in `echo "$FOLDERS" | egrep -oe '[a-zA-Z0-9_./-]*'`;do
+    echo " * Grabbing folder * $FOLDER"
+    REMOTEFOLDER=`echo "$FOLDER" | tr '/' '_'`
+    sudo mkdir -p "$FOLDER" 2>/dev/null
+    M=`whoami`
+    sudo chown ${M}:${M} "$FOLDER"
+    
+  done
 fi
 
 echo " * Done building local server from $ZSTORE backups!"
