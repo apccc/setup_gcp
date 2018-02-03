3fd4cff1df420f954706fc4d31c657bb2574afbd
Abraham P Chaffin <abraham@apc.cc>
2018-02-03 14:52:37 -0800

    Update setupServerSites.sh

--- scripts/buildLocalServer/apache2Web/setupServerSites.sh
+++ scripts/buildLocalServer/apache2Web/setupServerSites.sh
@@ -4,9 +4,14 @@ source ~/setup-config/setup_gcp/core.sh
 
 echo " * Setting up Server Sites"
 
-for SERVERWEBSITEID in $($MY 'SELECT `id` FROM `'"$SYSTEM_DATABASE"'`.`sites` WHERE `subdomain`!="'"${COMPANY_ADMIN_SUBDOMAIN}.${COMPANY_DOMAIN}"'" AND `active`="T" ORDER BY `subdomain` ASC' | tail -n +2);do
-  SUBDOMAIN=$($MY 'SELECT `subdomain` FROM `'"$SYSTEM_DATABASE"'`.`sites` WHERE `id`="'"$SERVERWEBSITEID"'" LIMIT 1' | tail -n 1)
-  ~/setup_gcp/scripts/buildLocalServer/apache2Web/setupSite.sh "${SUBDOMAIN}"
+VMID=`$MY "SELECT id FROM ${SYSTEM_DATABASE}.googleComputeEngine_VMInstances WHERE name='$(hostname)' LIMIT 1" | tail -n 1`
+
+for SITEID in $($MY 'SELECT `site_id` FROM `'"$SYSTEM_DATABASE"'`.`sites_vms` WHERE `vm_id`="'"${VMID}"'"' | tail -n +2);do
+  SERVERWEBSITEID=`$MY 'SELECT `id` FROM `'"$SYSTEM_DATABASE"'`.`sites` WHERE `subdomain`!="'"${COMPANY_ADMIN_SUBDOMAIN}.${COMPANY_DOMAIN}"'" AND `active`="T" AND `id`="'"${SITEID}"'"' | tail -n 1`
+  if [ ! -z "$SERVERWEBSITEID" ];then
+    SUBDOMAIN=$($MY 'SELECT `subdomain` FROM `'"$SYSTEM_DATABASE"'`.`sites` WHERE `id`="'"$SERVERWEBSITEID"'" LIMIT 1' | tail -n 1)
+    ~/setup_gcp/scripts/buildLocalServer/apache2Web/setupSite.sh "${SUBDOMAIN}"
+  fi
 done
 
 exit 0
