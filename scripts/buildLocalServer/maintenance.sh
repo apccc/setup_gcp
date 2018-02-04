84d1927c9aeea1e3b08a46217cafd7cdf8d252ec
Abraham P Chaffin <abraham@apc.cc>
2018-02-03 18:54:50 -0800

    Update maintenance.sh

--- scripts/buildLocalServer/maintenance.sh
+++ scripts/buildLocalServer/maintenance.sh
@@ -28,8 +28,11 @@ fi
 #create backup schedule for home path
 HOMEBID=`$MY "SELECT id FROM ${SYSTEM_DATABASE}.googleCloudStorage_buckets WHERE name='$HOMEBNAME' LIMIT 1" | tail -n +2`
 if [[ $HOMEBID -gt 0 ]] && [[ `$MY "SELECT id FROM ${SYSTEM_DATABASE}.googleCloudStorage_backupSchedule WHERE server='$(hostname)' AND path='/home'" | tail -n +2 | wc -l` -lt 1 ]];then
+  echo " * Creating backup schedule for /home directory on $(hostname) to $HOMEBNAME (${HOMEBID})"
   X='INSERT INTO `'"${SYSTEM_DATABASE}"'`.`googleCloudStorage_backupSchedule` (`server`,`path`,`bucket_id`,`nextRun`) '
   X=$X'VALUES ("'"$(hostname)"'","/home","$HOMEBID",NOW());'
+  echo
+  echo "Running: $X"
   $MY "$X"
 fi
 
