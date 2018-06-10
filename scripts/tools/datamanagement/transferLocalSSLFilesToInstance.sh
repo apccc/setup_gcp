a1a3fdec1337e66f05aa28555d9cee3459eed72d
Abraham P Chaffin <abraham@apc.cc>
2018-06-09 18:36:46 -0700

    Update transferLocalSSLFilesToInstance.sh

--- scripts/tools/datamanagement/transferLocalSSLFilesToInstance.sh
+++ scripts/tools/datamanagement/transferLocalSSLFilesToInstance.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
+#Transfer local SSL files in the /root/ssl directory to a remote instance
+
+if [ -z "$1" ];then
+  echo "Error: Remote instance name not provided."
+  exit 1
+fi
+INSTANCE=`echo "$1" | egrep -oe '[a-zA-Z0-9_.-]'`
+
+echo "Transferring SSL files to instance $INSTANCE"
+
+exit 0
+mkdir /tmp/ssl
+sudo rsync -av /root/ssl/ /tmp/ssl/
+gcloud compute scp --recurse /tmp/ssl $INSTANCE:~/
+rm -r /tmp/ssl
+
 exit 0
