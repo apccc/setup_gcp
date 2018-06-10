23c1eb6b9d04c39f06f2b0b0581c3b2c27695d83
Abraham P Chaffin <abraham@apc.cc>
2018-06-10 09:42:23 -0700

    Making file executable.

--- scripts/tools/datamanagement/transferLocalFilesToInstance.sh
+++ scripts/tools/datamanagement/transferLocalFilesToInstance.sh
@@ -2,10 +2,16 @@
 
 #Transfer Local Files To Instance
 
+if [ -z "$1" ];then
+  echo "Error: Remote instance name not provided."
+  exit 1
+fi
+INSTANCE=`echo "$1" | egrep -oe '[a-zA-Z0-9_.-]*' | head -n 1`
+
 LOCALDIR=`dirname $0`
 
-$LOCALDIR/transferLocalSSLFilesToInstance.sh
-$LOCALDIR/transferLocalDatabaseBackupToInstance.sh
-$LOCALDIR/transferLocalWebFilesToInstance.sh
+$LOCALDIR/transferLocalSSLFilesToInstance.sh "$INSTANCE"
+$LOCALDIR/transferLocalDatabaseBackupToInstance.sh "$INSTANCE"
+$LOCALDIR/transferLocalWebFilesToInstance.sh "$INSTANCE"
 
 exit 0
