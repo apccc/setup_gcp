4b3ef421fd53da58dbb6e5baa81f62539ac05694
Abraham P Chaffin <abraham@apc.cc>
2018-06-09 18:53:54 -0700

    Update transferLocalSSLFilesToInstance.sh

--- scripts/tools/datamanagement/transferLocalSSLFilesToInstance.sh
+++ scripts/tools/datamanagement/transferLocalSSLFilesToInstance.sh
@@ -7,13 +7,15 @@ if [ -z "$1" ];then
   exit 1
 fi
 INSTANCE=`echo "$1" | egrep -oe '[a-zA-Z0-9_.-]*' | head -n 1`
+DESTDIR="/tmp/"
+PROCESSMESSAGE="Transferring SSL files to instance $INSTANCE at $DESTDIR"
+echo "$PROCESSMESSAGE"
 
-echo "Transferring SSL files to instance $INSTANCE"
-
-exit 0
 mkdir /tmp/ssl
 sudo rsync -av /root/ssl/ /tmp/ssl/
-gcloud compute scp --recurse /tmp/ssl $INSTANCE:/tmp/
-rm -r /tmp/ssl
+gcloud compute scp --recurse /tmp/ssl $INSTANCE:$DESTDIR
+sudo rm -r /tmp/ssl
+
+echo "Done $PROCESSMESSAGE"
 
 exit 0
