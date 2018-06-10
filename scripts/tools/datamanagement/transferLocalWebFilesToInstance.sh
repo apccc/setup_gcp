adc275aa653306ad0d7e29a717df70b5d0fff1b7
Abraham P Chaffin <abraham@apc.cc>
2018-06-09 20:09:24 -0700

    Making file executable.

---
+++ scripts/tools/datamanagement/transferLocalWebFilesToInstance.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#Transfer local Web files in the /var/www directory to a remote instance
+
+if [ -z "$1" ];then
+  echo "Error: Remote instance name not provided."
+  exit 1
+fi
+INSTANCE=`echo "$1" | egrep -oe '[a-zA-Z0-9_.-]*' | head -n 1`
+DESTDIR="/var/www/"
+LOCALDIR="/var/www"
+PROCESSMESSAGE="Transferring Web files to instance $INSTANCE at $DESTDIR"
+echo " * $PROCESSMESSAGE"
+
+for SITEFOLDERPATH in `ls -1d $LOCALDIR/*`;do
+  echo " * Transferring $SITEFOLDERPATH to $INSTANCE:$DESTDIR"
+  gcloud compute scp --recurse "$SITEFOLDERPATH" $INSTANCE:$DESTDIR
+done
+
+echo " * Done $PROCESSMESSAGE"
+
+exit 0
