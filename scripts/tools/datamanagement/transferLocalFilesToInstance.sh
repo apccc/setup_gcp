49048d8ef87ce60c6fcb9ede31d164c549038305
Abraham P Chaffin <abraham@apc.cc>
2018-06-10 09:41:33 -0700

    Update transferLocalFilesToInstance.sh

--- scripts/tools/datamanagement/transferLocalFilesToInstance.sh
+++ scripts/tools/datamanagement/transferLocalFilesToInstance.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+#Transfer Local Files To Instance
+
+LOCALDIR=`dirname $0`
+
+$LOCALDIR/transferLocalSSLFilesToInstance.sh
+$LOCALDIR/transferLocalDatabaseBackupToInstance.sh
+$LOCALDIR/transferLocalWebFilesToInstance.sh
+
 exit 0
