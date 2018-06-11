4e87bc424d7449291d8ca804cc2eb147df8afe4a
Abraham P Chaffin <abraham@apc.cc>
2018-06-10 17:52:14 -0700

    Update setupPHP.ini.sh

--- scripts/buildLocalServer/apache2Web/setupPHP.ini.sh
+++ scripts/buildLocalServer/apache2Web/setupPHP.ini.sh
@@ -2,7 +2,9 @@
 source ~/setup-config/setup_gcp/core.sh
 
 PHPINI=`sudo find /etc/ -name "php.ini" | grep 'apache' | head -n 1`
+PHPINICLI=`sudo find /etc/ -name "php.ini" | grep 'cli' | head -n 1`
 AUTO_PREPEND_FILE="/var/www/auto_preprend_file.php"
+TMPFILE=/tmp/setupphpini.txt
 
 if [ ! -f "$PHPINI" ];then
   echo "Error: Could not find php.ini at $PHPINI"
@@ -24,10 +26,22 @@ echo '$MAILGUN_SMTP_USERNAME="'"$MAILGUN_SMTP_USERNAME"'";' | sudo tee -a "$F" >
 echo '$MAILGUN_SMTP_PASSWORD="'"$MAILGUN_SMTP_PASSWORD"'";' | sudo tee -a "$F" > /dev/null
 echo '?>' | sudo tee -a $F > /dev/null
 
-#perform updates on php.ini file
-TMPFILE=/tmp/setupphpini.txt
+#perform updates on apache php.ini file
 cp "$PHPINI" "$TMPFILE"
 $FR "auto_prepend_file =" "auto_prepend_file = $AUTO_PREPEND_FILE" "$TMPFILE"
 sudo cp "$TMPFILE" "$PHPINI"
 rm "$TMPFILE"
+
+#update the PHP CLI as a courtesy
+if [ ! -f "$PHPINICLI" ];then
+  echo "Error: Could not find CLI php.ini at $PHPINICLI"
+  exit 1
+fi
+
+#perform updates on apache php.ini file
+cp "$PHPINICLI" "$TMPFILE"
+$FR "auto_prepend_file =" "auto_prepend_file = $AUTO_PREPEND_FILE" "$TMPFILE"
+sudo cp "$TMPFILE" "$PHPINICLI"
+rm "$TMPFILE"
+
 exit 0
