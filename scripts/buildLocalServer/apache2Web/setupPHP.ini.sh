1e64daa82e818497762370956f48135465e581cf
Abraham P Chaffin <abraham@apc.cc>
2018-10-27 20:25:35 -0700

    Update setupPHP.ini.sh
    Making the default database AES key available to the Web server for use. Restricting access on PHP file.

--- scripts/buildLocalServer/apache2Web/setupPHP.ini.sh
+++ scripts/buildLocalServer/apache2Web/setupPHP.ini.sh
@@ -16,10 +16,13 @@ echo " * Configuring php.ini"
 #setup the auto_prepend_file
 F="$AUTO_PREPEND_FILE"
 echo '<?php' | sudo tee "$F" > /dev/null
+sudo chown www-data "$F"
+sudo chmod 600 "$F"
 echo '$SYSTEM_DATABASE="'"$SYSTEM_DATABASE"'";' | sudo tee -a "$F" > /dev/null
 echo '$MYSQL_USER="'"$MYSQL_WEB_USER"'";' | sudo tee -a "$F" > /dev/null
 echo '$MYSQL_PASS="'"$MYSQL_WEB_USER_PASS"'";' | sudo tee -a "$F" > /dev/null
 echo '$MYSQL_1_HOST="'"$MYSQL_1_HOST"'";' | sudo tee -a "$F" > /dev/null
+echo '$MYSQL_AES_KEY="'"$MYSQL_AES_KEY"'";' | sudo tee -a "$F" > /dev/null
 echo '$RECAPTCHA_SITE_KEY="'"$RECAPTCHA_SITE_KEY"'";' | sudo tee -a "$F" > /dev/null
 echo '$RECAPTCHA_SECRET_KEY="'"$RECAPTCHA_SECRET_KEY"'";' | sudo tee -a "$F" > /dev/null
 echo '$MAILGUN_SMTP_USERNAME="'"$MAILGUN_SMTP_USERNAME"'";' | sudo tee -a "$F" > /dev/null
